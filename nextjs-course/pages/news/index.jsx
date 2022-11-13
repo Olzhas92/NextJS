2e978845f4b9f3b0305cf9f18b2468b86b34dd64
olzhaskhamzin <olzhas.khamzin@gmail.com>
2022-11-13 16:05:16 +0600

    327. Linking Between Pages

--- nextjs-course/pages/news/index.jsx
+++ nextjs-course/pages/news/index.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Link from "next/link";
 
 const NewsPage = () => {
-  return <h2>The News Page</h2>;
+  return (
+    <Fragment>
+      <h2>The News Page</h2>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great framework
+          </Link>
+        </li>
+        <li>
+          <Link href="/news/something-else">Something else</Link>
+        </li>
+      </ul>
+    </Fragment>
+  );
 };
 
 export default NewsPage;
